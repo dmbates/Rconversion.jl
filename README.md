315fa73ea015bec1bf7701fff4bdacf6d1c83b51
Douglas Bates <dmbates@gmail.com>
2025-11-02 15:48:59 -0800

    Messing around with initializing of Const

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Rconversion
 
 [![Build Status](https://github.com/dmbates/Rconversion.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/dmbates/Rconversion.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+## Conversion of R objects to Julia objects and vice-versa
+
+Most of the code in this package is from [RCall.jl](https://github.com/JuliaInterop/RCall.jl).
+
+The plan is for the conversion code to be isolated in this package, which will be included by `RCall.jl`.
+It can also be included by packages that will be compiled to shared (or 'dynamic') libraries to be loaded into an R session.
--- src/Rconversion.jl
+++ src/Rconversion.jl
@@ -19,6 +19,12 @@ import Base.Iterators: iterate, IteratorSize, IteratorEltype, Pairs, pairs
 
 const libR = "/Library/Frameworks/R.framework/Resources/lib/libR"
 
+# const libR = Base.OncePerProcess{String}() do
+#     lR = only(filter(contains("libR."), Libc.Libdl.dllist()))
+#     println(Core.stdout, "libR = ", lR)
+#     return lR
+# end
+
 include("types.jl")
 include("Const.jl")
 Const.load(false) # load the values of constants assuming that libR has been dlopen'd
@@ -35,8 +41,10 @@ export
     NilSxp,
     RealSxp,
     StrSxp,
+    SymSxp,
     NilValue,
     RObject,
+    getattrib,
     rcopy,
     sexp,
     preserve,
